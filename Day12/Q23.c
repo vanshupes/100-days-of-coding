cc02298e9eb884cb2ebb1beced61e09de1cf8007
amdynamo05 <amdynamo05@gmail.com>
2026-09-22 16:51:54 +0530

    Create Q23.c

---
+++ Day12/Q23.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main()
+{
+int days,fine;
+printf("Enter late days: ");
+scanf("%d",&days);
+if(days<=5)
+{
+fine=days*2;
+printf("Fine= %drs",fine);
+}
+else if(days<=10)
+{
+fine=(5*2)+(days-5)*4;
+printf("Fine= %drs",fine);
+}
+else if(days<=30)
+{
+fine=(5*2)+(5*4)+(days-10)*6;
+printf("Fine= %drs",fine);
+}
+else
+printf("Membership Cancelled");
+return 0;
+}
