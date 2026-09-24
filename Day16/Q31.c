7b5cae7d0b1cf2be69c29cfde089272732f62712
amdynamo05 <amdynamo05@gmail.com>
2026-09-24 18:20:16 +0530

    Create Q31.c

---
+++ Day16/Q31.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+int n,rem,binary=0,place=1;
+printf("Enter a number: ");
+scanf("%d",&n);
+while(n>0)
+{
+rem=n%2;
+binary=binary+rem*place;
+place=place*10;
+n=n/2;
+}
+printf("Binary= %d",binary);
+return 0;
+}
